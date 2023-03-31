3ba0ef7c67a30581de99c871e9e61253b8389660
EMANU02004 <masambue45@gmail.com>
2023-03-31 00:13:59 -0700

    Updated more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,36 +1,32 @@
 #include "main.h"
 
 /**
- * _strncpy - A function that copies a string.
- *
- * @dest: The destination string
- * @src: The string to copy
- * @n: Number of times to copy
- *
- * Return: The copy of the src and append it to the dest
- * n number of times.
- */
+  * _strncat - A function that concatenates two strings
+  *
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The limit of the concatenation
+  *
+  * Return: A pointer to the resulting string dest
+  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int join = 0, i = 0;
+	int concat = 0, i = 0;
 
-	while (src[i])
+	while (dest[concat])
 	{
+		concat++;
+	}
+
+	while (i < n && src[i])
+	{
+		dest[concat] = src[i];
+		concat++;
 		i++;
 	}
 
-	while (join < n && src[join])
-	{
-		dest[join] = src[join];
-		join++;
-	}
-
-	while (join < n)
-	{
-		dest[join] = '\0';
-		join++;
-	}
+	dest[concat + n + 1] = '\0';
 
 	return (dest);
 }
