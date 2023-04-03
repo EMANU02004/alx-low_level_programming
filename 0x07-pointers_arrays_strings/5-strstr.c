4e867ea05f4f6acb5a587dcc5cb7dc7d41352159
EMANU02004 <masambue45@gmail.com>
2023-04-03 14:42:51 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strstr - Entry Point
+ * @haystack: String to be searched
+ * needle: Locate substring
+ * Return: If located substring - pointer to the beginning of substring
+ * not located - NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int index;
+
+if (*needle == 0)
+	return (haystack);
+
+while (*haystack)
+{
+	index = 0;
+
+	if (haystack[index] == needle[index])
+	{
+		do {
+			if (needle[index + 1] == '\0')
+			return (haystack);
+			index++;
+
+		} while (haystack[index] == needle[index]);
+	}
+	haystack++;
+}
+return ('\0');
+}
+
