054484de335c1f9c0ae09b977d0f85a9c2b3c156
EMANU02004 <masambue45@gmail.com>
2023-08-17 04:50:59 -0700

    Doubly.py

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Bamidele Adefolaju
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
